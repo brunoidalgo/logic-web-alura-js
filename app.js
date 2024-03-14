eea36a52b557613e9b73092c1900d90bc1b9f7fb
SMARAPD - Bruno Empke <BEMPKE@SMARAPD.COM.BR>
2024-03-14 18:05:29 -0300

    Task: Adicionando lógica do jogo

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10:';
-
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
 
 function verificarChute () {
-  
+  let chute = document.querySelector('input').value;
+  if (numeroSecreto == chute) {
+    exibirTextoNaTela('p', `Você acertou o número era: ${numeroSecreto}`)
+  } else {
+    exibirTextoNaTela('p', `Você errou o número era: ${numeroSecreto}`)
+  }
+}
+
+function novoJogo(){
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
+}
+
+function exibirTextoNaTela(tag,texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10:');
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
 }
